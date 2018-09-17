5ba2e19af1b6f220611e7157d013d510cccce6f7
ArtemZaZ <temka.911@mail.ru>
2018-09-17 16:08:58 +0300

    Поправил графики

--- Plot.py
+++ Plot.py
@@ -4,8 +4,7 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 from matplotlib.figure import Figure
-from matplotlib.backends.backend_gtk3 import (
-    NavigationToolbar2GTK3 as NavigationToolbar)
+from matplotlib.backends.backend_gtk3 import NavigationToolbar2GTK3
 from matplotlib.backends.backend_gtk3agg import (
     FigureCanvasGTK3Agg as FigureCanvas)
 
@@ -17,9 +16,11 @@ class SnaptoCursor(object):
     """
 
     def __init__(self, ax, x, y):  # ax - plot, x - набор данных по x, y - набор данных по y
+        self.__active = False  # активен ли курсор
         self.ax = ax
         self.lx = ax.axhline(color='k', lw=1)  # the horiz line
         self.ly = ax.axvline(color='k', lw=1)  # the vert line
+        self.active = False     # убираем линии
         self.x = x
         self.y = y
 
@@ -31,9 +32,21 @@ class SnaptoCursor(object):
         x = self.x[indx]
         y = self.y[indx]
         # update the line positions
-        self.lx.set_ydata(y)
-        self.ly.set_xdata(x)
-        self.ax.figure.canvas.draw()    # обновляем данные график
+        if self.__active:
+            self.lx.set_ydata(y)
+            self.ly.set_xdata(x)
+        self.ax.figure.canvas.draw()  # обновляем данные график
+
+    @property
+    def active(self):
+        return self.__active
+
+    @active.setter
+    def active(self, a: bool):
+        self.__active = a
+        if not a:
+            self.lx.set_ydata(None)    # убираем линии, чтоб их было не видно
+            self.ly.set_xdata(None)
 
 
 # виджет-график
@@ -50,6 +63,8 @@ class PlotCanvas(FigureCanvas):
         self.data = data
         self.plot = self.fig.add_subplot(1, 1, 1)
         self.plot.plot(self.data.measurementNumber, self.data.magnitudeX)  # сделать, чтоб было 3 графика
+        self.plot.plot(self.data.measurementNumber, self.data.magnitudeY)
+        self.plot.plot(self.data.measurementNumber, self.data.magnitudeZ)
         self.cursor = SnaptoCursor(self.plot, self.data.measurementNumber, self.data.magnitudeX)
         self.fig.canvas.mpl_connect("motion_notify_event", self.cursor.mouseMove)  # привязываем события к обработчикам
         self.fig.canvas.mpl_connect("button_press_event", self.mousePress)
@@ -57,13 +72,19 @@ class PlotCanvas(FigureCanvas):
     def mousePress(self, event):  # обработчик события, нажатия кнопки мыши
         if not event.inaxes:
             return
-        x, y = self.cursor.ly.get_xdata(), self.cursor.lx.get_ydata()  # получение текущих значений вертикальной и
-        # горизонтальной линий
-        self.plot.scatter(x, y, s=50)  # ставим маркер на график, толщина 50 попугаев
-        self.setMarker(self, self.cursor, x, y)  # вызываем ф-ию обработчик того, что мы поставили маркер
+        if self.cursor.active:
+            x, y = self.cursor.ly.get_xdata(), self.cursor.lx.get_ydata()  # получение текущих значений вертикальной и
+            # горизонтальной линий
+            self.plot.scatter(x, y, s=50)  # ставим маркер на график, толщина 50 попугаев
+            self.setMarker(self, self.cursor, x, y)  # вызываем ф-ию обработчик того, что мы поставили маркер
 
     def setMarker(self, cursor, x, y):  # ф-ия для перегрузки, вызывается, когда ставится маркер
         pass
+
+
+class NavigationToolbar(NavigationToolbar2GTK3):
+    toolitems = [tool for tool in NavigationToolbar2GTK3.toolitems if
+                 tool[0] in ('Home', 'Back', 'Forward', 'Pan', 'Zoom', 'Save')]
 
 
 class PlotWindow(Gtk.Window):  # отдельное окно с графиком
@@ -71,11 +92,33 @@ class PlotWindow(Gtk.Window):  # отдельное окно с графиком
         Gtk.Window.__init__(self, title=title)
         self.set_property("width_request", 300)
         self.set_property("height_request", 300)
-        self.plotCanvas = PlotCanvas()
+        self.plotCanvas = PlotCanvas()  # график
+        self.toolbar = NavigationToolbar(self.plotCanvas, self)  # toolbar matplotlib'a
+
+        self.markerImage = Gtk.Image()  # изображение маркера
+        self.markerImage.set_from_file("images/marker25x25.png")
+        self.rebuildImage = Gtk.Image(stock=Gtk.STOCK_EXECUTE)  # изображение шестеренок
+
+        self.rebuildButton = Gtk.Button(label=None, image=self.rebuildImage)  # кнопка перекомпановки графиков
+
+        self.setMarkerToggleButton = Gtk.ToggleButton(label=None, image=self.markerImage)  # кнопка-переключатель,
+        # при нажании на которую можно ставить маркеры
+        self.setMarkerToggleButton.connect("toggled", self.setMarkerToggleButton_Click)
+
+        self.customToolBox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=2)
+        self.customToolBox.pack_start(self.toolbar, False, False, 1)
+        self.customToolBox.pack_start(self.setMarkerToggleButton, False, False, 0)
+        self.customToolBox.pack_start(self.rebuildButton, False, False, 0)
+
         self.box = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=2)
-        self.add(self.box)
         self.box.pack_start(self.plotCanvas, True, True, 0)
-        self.toolbar = NavigationToolbar(self.plotCanvas, self)
-        self.box.pack_start(self.toolbar, False, False, 1)
+        self.box.pack_start(self.customToolBox, False, False, 1)
+        self.add(self.box)
+
+    def setMarkerToggleButton_Click(self, button):
+        self.plotCanvas.cursor.active = button.get_active()     # делаем курсор активным/неактивным
 
 
+if __name__ == "__main__":
+    PlotWindow().show_all()
+    Gtk.main()
